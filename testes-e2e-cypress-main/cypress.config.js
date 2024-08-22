e869666f59ec1b6aed36ffe59329768b27ed6b1b
Henrique Coelho Vargas <coelhohenrique.git@gmail.com>
2024-08-22 09:37:21 -0300

    test, teste email invalido

--- testes-e2e-cypress-main/cypress.config.js
+++ testes-e2e-cypress-main/cypress.config.js
@@ -2,9 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    e2e: {
-      baseUrl: 'http://localhost:3000' }, 
+    setupNodeEvents(on, config){
+      
     }
-});
+  }
+})
+
+module.exports = defineConfig({
+    e2e: {
+      baseUrl: 'http://localhost:3000' 
+    }, 
+  });
 
 
