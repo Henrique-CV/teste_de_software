1fd038de0ffe477979424ea4481e6f7e97461b83
Henrique Coelho Vargas <coelhohenrique.git@gmail.com>
2024-08-23 14:07:34 -0300

    test, jornada completa

--- testes-e2e-cypress-main/cypress/e2e/jornadaUsuario.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/jornadaUsuario.cy.js
@@ -5,16 +5,35 @@ describe('Jornadas de usuário', () => {
     cy.getByData('email-input').type('neilton@alura.com')
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
+
+    //paginas
     cy.location('pathname').should('eq','/home')
-    cy.getByData('select-opcoes').select('Transferência') /* no formulário, 'select-opcoes' é o campo onde selecionamos qual tipo de transação. Escolhemos 'Transferência' */
-    cy.getByData('form-input').type('80') /* incluímos um valor para a transferência*/
+
+    cy.getByData('app-home').find('a').eq(1).click()
+    cy.location('pathname').should('eq','/home/cartoes')
+    cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
+
+    cy.getByData('app-home').find('a').eq(2).click()
+    cy.location('pathname').should('eq','/home/servicos')
+    cy.getByData('div-servicos').should('exist').and('be.visible')
+
+    cy.getByData('app-home').find('a').eq(3).click()
+    cy.location('pathname').should('eq','/home/investimentos')
+    cy.getByData('titulo-investimentos').should('exist').and('have.text', 'Investimentos')
+
+    //tranferencia
+    cy.getByData('app-home').find('a').eq(0).click()
+    cy.getByData('select-opcoes').select('Transferência')
+    cy.getByData('form-input').type('80') 
     cy.getByData('realiza-transacao').click()
-    cy.getByData('lista-transacoes').find('li').last().contains('- R$ 80') /* no histórico de transações (ul), encontraremos um li, o último, que contenha o valor que inserimos */
+    cy.getByData('lista-transacoes').find('li').last().contains('- R$ 80') 
+
+    //deposito
     cy.getByData('select-opcoes').select('Depósito')
-    cy.getByData('form-input').type('80') /* incluímos um valor para a transferência*/
+    cy.getByData('form-input').type('80')
     cy.getByData('realiza-transacao').click()
     cy.getByData('lista-transacoes').find('li').last().contains(' R$ 80')
-    
+
     cy.getByData('botao-sair').click()
     cy.location('pathname').should('eq','/')
     })
