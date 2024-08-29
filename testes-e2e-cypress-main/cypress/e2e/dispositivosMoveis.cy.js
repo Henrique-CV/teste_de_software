dc242f000b19e511656c2d9c72e2164b7fae2422
Henrique Coelho Vargas <coelhohenrique.git@gmail.com>
2024-08-29 10:56:42 -0300

    test, testando no iphone

--- testes-e2e-cypress-main/cypress/e2e/dispositivosMoveis.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/dispositivosMoveis.cy.js
@@ -16,3 +16,45 @@ describe('Testando dispositivos móveis', () => {
         cy.location('pathname').should('eq','/home/investimentos')			
     })
 })
+describe('Menu de navegação', () => {
+    context('Resolução de 720p', () => {
+      beforeEach(() => {
+        /* Roda os testes como se fossem em um monitor de 720p de resolução */
+        cy.viewport(1280, 720)
+      })
+
+      /* seu teste aqui */
+      it('Deve existir um menu burguer', () => {
+        cy.visit('/')
+        
+        cy.viewport(375, 667)
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        
+        cy.location ('pathname').should('eq','/home')
+      })
+    })
+
+    context('Resolução do iphone-5 ', () => {
+      beforeEach(() => {
+        /* roda os testes como se fossem em um dispositivo com a resolução de um iphone-5 */
+        cy.viewport('iphone-5')
+      })
+
+      /* seu teste aqui */
+      it('Deve existir um menu burguer', () => {
+        cy.visit('/')
+        
+        cy.viewport(375, 667)
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        
+        cy.location ('pathname').should('eq','/home')
+        cy.viewport('iphone-5', 'landscape')
+      })
+    })
+  })
